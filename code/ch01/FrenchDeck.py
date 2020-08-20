a6889c761883395fd1cb33bf1fcc0c7f2fe58ed1
fbacinello <francobacinello22@gmail.com>
2020-08-20 10:33:54 +0200

    Primer commit.

---
+++ code/ch01/FrenchDeck.py
@@ -0,0 +1,35 @@
+import collections
+
+Card = collections.namedtuple('Card', ['rank', 'suit'])
+
+
+class FrenchDeck:
+    ranks = [str(n) for n in range(2, 11)] + list('JQKA')
+    suits = 'spades diamonds clubs hearts'.split()
+
+    def __init__(self):
+        self._cards = [Card(rank, suit)
+                       for suit in self.suits
+                       for rank in self.ranks]
+
+    def __len__(self):
+        return len(self._cards)
+
+    def __getitem__(self, position):
+        return self._cards[position]
+
+
+# Should we create a method to pick a random card? No need. Python already has a
+# function to get a random item from a sequence: random.choice
+
+from random import choice
+
+deck = FrenchDeck()
+choice(deck)
+
+suit_values = dict(spades=3, hearts=2, diamonds=1, clubs=0)
+
+
+def spades_high(card):
+    rank_value = FrenchDeck.ranks.index(card.rank)
+    return rank_value * len(suit_values) + suit_values[card.suit]
